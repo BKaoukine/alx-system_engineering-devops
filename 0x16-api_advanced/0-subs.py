fd74edb24d50900f9d40655b9cc9abe33e5b76f0
bkaoukine <b.koukin@gmail.com>
2024-04-10 18:56:00 +0000

    0. How many subs?

--- 0x16-api_advanced/0-subs.py
+++ 0x16-api_advanced/0-subs.py
@@ -9,10 +9,19 @@ import sys
 
 def number_of_subscribers(subreddit):
     """number_of_subscribers returns a number of subscribers."""
-    url = f"https://www.reddit.com/r/{subreddit}/about.json"
-    resp = requests.get(url, allow_redirects=False)
+    username = 'ledbag123'
+    password = 'Reddit72'
+
+    user_pass_dict = {'user': username, 'passwd': password, 'api_type': 'json'}
+    headers = {'user-agent': '/u/ledbag123 API Python for Holberton School'}
+
+    url = 'https://www.reddit.com/r/{}/about.json'.format(subreddit)
+
+    client = requests.session()
+    client.headers = headers
+
+    resp = client.get(url, allow_redirects=False)
     if resp.status_code == 200:
-        data = resp.json()
-        return data['data']['subscribers']
+        return (resp.json()["data"]["subscribers"])
     else:
-        return 0
+        return(0)
