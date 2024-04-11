26042a2f45bfb20d20701c924ef5e958fb96ea91
bkaoukine <b.koukin@gmail.com>
2024-04-11 00:33:11 +0000

    1. Top Ten

---
+++ 0x16-api_advanced/1-top_ten.py
@@ -0,0 +1,28 @@
+#!/usr/bin/python3
+"""Top_Ten function that queries the Reddit.
+
+API and returns the top 10 hot posts of a given subreddit.
+"""
+
+
+import requests
+import sys
+headers = {"User-Agent": "MyCustomUserAgent/1.0"}
+
+
+def top_ten(subreddit):
+    """Top_Ten returns top 10 hot posts of a given subreddit."""
+    url = f"https://www.reddit.com/r/{subreddit}/comments.json?sort=hot"
+    response = requests.get(url, allow_redirects=False, headers=headers)
+    if response.status_code == 200:
+        data = response.json()
+        i = 0
+        for child in data['data']['children']:
+            link_title = child['data']['link_title']
+            print(link_title)
+            i = i + 1
+            if i == 10:
+                return
+
+    else:
+        print("None")
